3d29cc26046f6096870e6db3507b4a429d7492cc
AriadnaHCTec <A01749161@itesm.mx>
2020-11-27 23:33:32 -0600

    sql

--- subir_datos/subir_datos.ino
+++ subir_datos/subir_datos.ino
@@ -11,11 +11,22 @@ Programa para subir mediciones a la base de datos en sql.
 #define dht_dpin 0
 DHT dht(dht_dpin, DHTTYPE); 
 
+//Internet
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+const char *red = "INFINITUMC99E";//Ari: INFINITUMC99E
+const char *password = "FEw3Cp4M2j";//Ari: FEw3Cp4M2j
+String urlBase = "http://192.168.1.90/grabar/pruebainserta.php?fecha=2020-11-19&hora=22:05:20&valor=";  // GET?
+HTTPClient http;
+WiFiClient clienteWiFi;
 
 void setup() {
+  // Para depurar por el puerto serial
+  abrirSerial();
+  // Conectarse a la red
+  conectarARed();
   // Inicialización de temperatura.
   dht.begin();
-  Serial.begin();
 }
 
 void loop() {
@@ -32,12 +43,52 @@ void loop() {
   }
   for (int i = 89; i <= 99; i++){
     promedioTemperatura += temperaturas[i];
-    promedioHumedad += humedad[i];
+    promedioHumedad += humedades[i];
   }
   promedioTemperatura /= 10;
-  promedioHumedad /= 10;
+  promedioHumedad /= 10;  
 
+  // Armar la ruta del servicio
+  String url = String( urlBase + String(promedioTemperatura) );
+  Serial.println(url);
+
+  // Enviarlos por la red al servicio
+  // Solicitar la conexión al servicio
+  if ( http.begin(clienteWiFi, url) ) { 
+    int codigo = http.GET();  // Realizar petición
+    Serial.printf("Código: %d\n", codigo);
+      if (codigo == HTTP_CODE_OK || codigo == HTTP_CODE_MOVED_PERMANENTLY) {
+        String respuesta = http.getString();  // Respuesta
+        Serial.println(respuesta);
+      } else {
+      Serial.printf("GET falló, error: %s\n", http.errorToString(codigo).c_str());
+    }
+  } else {
+    Serial.println("No es posible hacer la conexión");
+  }
+  http.end();
+  delay(10000); // Cada 10 segundos
+
+}
+
+void abrirSerial() {
+  // Para la consola serial
+  Serial.begin(57600);
+  delay(1000);
+  Serial.println("\n");
+}
+
+// Conectarse a la red
+void conectarARed() {
+  Serial.println("\nConectando a la red");
+  WiFi.begin(red, password);
   
+  while (WiFi.status() != WL_CONNECTED) {
+    // Esperar medio segundo
+    delay(500);
+    Serial.print(".");
+  }
   
-  
+  // Ya se conectó, avisar
+  Serial.println("\nConectado!");
 }
