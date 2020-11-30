3078e66ebd08a0763ded3edf63e59d08d79205ab
luisferro2 <a01378248@itesm.mx>
2020-11-30 14:53:01 -0600

    Preprocesamiento y setup para sensor de oximetría.

--- subir_datos/subir_datos.ino
+++ subir_datos/subir_datos.ino
@@ -5,6 +5,19 @@ Programa para subir mediciones a la base de datos en sql.
 
 */
 
+// Preprocesamiento de oximetría
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+MAX30105 particleSensor; // Instancia de sensor.
+uint_32_t irBuffer[100]; // Para guardar del sensor infrarrojo.
+uint_32_t redBuffer[100]; // Guardar información del sensor rojo.
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
 // Preprocesamiento para temperatura.
 #include "DHT.h"
 #define DHTTYPE DHT11
@@ -14,19 +27,41 @@ DHT dht(dht_dpin, DHTTYPE);
 //Internet
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-const char *red = "INFINITUMC99E";//Ari: INFINITUMC99E
+const char *red = "INFINITUMC99E";//Ari: INFINITUMC99E  
+                                   // Red Luis Ferro: FerSal
 const char *password = "FEw3Cp4M2j";//Ari: FEw3Cp4M2j
 String urlBase = "http://192.168.1.90/IoT/insertaMedicion";  // GET? Ari: 192.168.1.90
+                                                             // Ip interna de compu Luis Ferro: 192.
+                                                              // La ip pública de Ari receptora es 189.225.66.113
 HTTPClient http;
 WiFiClient clienteWiFi;
 
 void setup() {
-  // Para depurar por el puerto serial
+  // Para depurar por el puerto serial.
   abrirSerial();
-  // Conectarse a la red
+  // Conectarse a la red.
   conectarARed();
   // Inicialización de temperatura.
   dht.begin();
+  // Inicializar sensor de oximetría.
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)){
+    Serial.println(F("No se encontró el sensor de oximetría"));
+    while(1); // Terminar.
+  }
+  // Esperar indicación de usuario para comenzar a medir.
+  Serial.println("\nColoque su sensor de oximetría contra su dedo firmemente. Cuando este listo mande byte a puerto serial.\n");
+  while(Serial.available() == 0){
+    Serial.read();  
+  }
+  // Setup de sensor.
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384  
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
 }
 
 void loop() {
@@ -59,30 +94,25 @@ void loop() {
 }
 
 //http://192.168.1.90/IoT/insertaMedicionFrecuenciaCardiaca.php?frecuenciaCardiaca=10&claveUsuario=1&modelo=MAX30102
-void enviarDatos(String tabla,float promedioMedicion ,String id, String modelo){
-  String url = String( urlBase + tabla+".php?"+decapitalize(tabla)+"="+String(promedioMedicion)+"&claveUsuario="+id+"&modelo="+modelo);
-
+void enviarDatos(String tabla, float promedioMedicion, String id, String modelo){
+  String url = String( urlBase + tabla + ".php?" + decapitalize(tabla) + "=" + String(promedioMedicion) + "&claveUsuario=" + id + "&modelo=" + modelo);
   Serial.println(url);
-
   // Enviarlos por la red al servicio
   // Solicitar la conexión al servicio
-  if ( http.begin(clienteWiFi, url) ) { 
+  if ( http.begin(clienteWiFi, url) ){ 
     int codigo = http.GET();  // Realizar petición
     Serial.printf("Código: %d\n", codigo);
-      if (codigo == HTTP_CODE_OK || codigo == HTTP_CODE_MOVED_PERMANENTLY) {
+      if (codigo == HTTP_CODE_OK || codigo == HTTP_CODE_MOVED_PERMANENTLY){
         String respuesta = http.getString();  // Respuesta
         Serial.println(respuesta);
-      } else {
+      } else{
       Serial.printf("GET falló, error: %s\n", http.errorToString(codigo).c_str());
     }
-  } else {
+  } else{
     Serial.println("No es posible hacer la conexión");
   }
   http.end();
 }
-
-
-
 
 void abrirSerial() {
   // Para la consola serial
@@ -95,13 +125,11 @@ void abrirSerial() {
 void conectarARed() {
   Serial.println("\nConectando a la red");
   WiFi.begin(red, password);
-  
   while (WiFi.status() != WL_CONNECTED) {
     // Esperar medio segundo
     delay(500);
     Serial.print(".");
   }
-  
   // Ya se conectó, avisar
   Serial.println("\nConectado!");
 }
