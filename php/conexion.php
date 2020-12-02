4ef40a94e38c62da598154e39447189bbfd91965
luisferro2 <a01378248@itesm.mx>
2020-12-01 20:53:32 -0600

    quitar echo en conexion para hacer select

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,6 @@
 <?php
 	try {
 		$pdo = new PDO('mysql:host=localhost;dbname=estacion_01', 'root', '');
-		echo "Conexión exitoso a la BD";
 	}catch(PDOException $e){
 		echo "<h1> Error conectando a BD</h1>";
 		print "Error, ".$e->getMessage();
